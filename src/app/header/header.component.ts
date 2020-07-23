3bb1187f0a4df2d2a8fd8bbd4d5115651ec13aa6
AhmedIbrahimHamed <aih1175@gmail.com>
2020-07-23 23:44:34 +0200

    Added Functionllity to Save Data and Fetch Data in Manga Dropdown

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: "app-header",
     templateUrl: "./header.component.html",
 })
 export class HeaderComponet {
+
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveDataHandler() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchRecipesHandler() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 
 }
