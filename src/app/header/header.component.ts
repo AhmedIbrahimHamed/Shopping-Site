ebff2c7e08a8fa7f37c1bbe7ac6c980f6f12bbd5
AhmedIbrahimHamed <aih1175@gmail.com>
2020-07-26 14:51:06 +0200

    update UI base on authentication

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: "app-header",
     templateUrl: "./header.component.html",
 })
-export class HeaderComponet {
+export class HeaderComponet implements OnInit,OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthenticated = user ? true : false;
+    });
+  }
 
   onSaveDataHandler() {
     this.dataStorageService.storeRecipes();
@@ -15,6 +25,10 @@ export class HeaderComponet {
 
   onFetchRecipesHandler() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
 }
