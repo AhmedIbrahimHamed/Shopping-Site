de7206348ce0d9dce43676d3dced4132e26e0417
AhmedIbrahimHamed <aih1175@gmail.com>
2020-07-29 00:54:52 +0200

    Added authGuard for authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -17,7 +17,7 @@ export interface AuthResponseData {
 @Injectable({ providedIn: "root" })
 export class AuthService {
   user = new BehaviorSubject<User>(null);
-  private tokenExpirationTimer : any;
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -48,24 +48,22 @@ export class AuthService {
       email: string;
       id: string;
       _token: string;
-      _tokenExprationData: string;
+      _tokenExpirationDate: string;
     } = JSON.parse(localStorage.getItem("userData"));
     if (!userData) {
       return;
-    } else {
-      const loadedUser = new User(
-        userData.email,
-        userData.id,
-        userData._token,
-        new Date(userData._tokenExprationData)
-      );
+    }
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
 
-      if (loadedUser.token) {
-        this.user.next(loadedUser);
-        const expirationDuration = new Date(userData._tokenExprationData).getTime() - new Date().getTime();
-        this.autoLogout(expirationDuration);
-      }
-
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+      this.autoLogout(expirationDuration);
     }
 
   }
